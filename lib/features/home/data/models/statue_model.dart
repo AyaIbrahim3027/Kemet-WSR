4ddc5b1295c1b7aa30f08e6421edd6b0d5c6c641
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-07-15 17:33:42 +0200

    create Statue Model

---
+++ lib/features/home/data/models/statue_model.dart
@@ -0,0 +1,39 @@
+class StatueModel {
+  final String name;
+  final String epoch;
+  final String image;
+  final String civilizationName;
+  final String ar;
+  final String ai;
+
+  StatueModel({
+    required this.name,
+    required this.epoch,
+    required this.image,
+    required this.civilizationName,
+    required this.ar,
+    required this.ai,
+  });
+
+  factory StatueModel.fromJson(Map<String, dynamic> json) {
+    return StatueModel(
+      name: json['name'] as String,
+      epoch: json['epoch'] as String,
+      image: json['image'] as String,
+      civilizationName: json['civilizationName'] as String,
+      ar: json['ar'] as String,
+      ai: json['ai'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'epoch': epoch,
+      'image': image,
+      'civilizationName': civilizationName,
+      'ar': ar,
+      'ai': ai,
+    };
+  }
+}
