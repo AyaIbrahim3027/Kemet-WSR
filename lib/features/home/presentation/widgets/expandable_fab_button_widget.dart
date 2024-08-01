4643cc546c8eeb86580a6c4e398697aa7c026e64
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-08-01 23:15:21 +0200

    Update chat in details view

--- lib/features/home/presentation/widgets/expandable_fab_button_widget.dart
+++ lib/features/home/presentation/widgets/expandable_fab_button_widget.dart
@@ -2,10 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kemetwsr/core/utils/resources/assets_manager.dart';
 import 'package:kemetwsr/core/utils/resources/strings_manager.dart';
 import 'package:kemetwsr/features/home/data/models/statue_model.dart';
+import '../../../../app/app_router.dart';
 import '../../../../core/utils/functions/buildRotateFloatingActionButtonBuilder.dart';
+import '../../../../core/utils/resources/data.dart';
 import '../../../../core/utils/widgets/button_widget.dart';
 import '../../../../core/utils/widgets/floating_action_button_widget.dart';
 import '../../../AR/presentation/views/launch_AR.dart';
@@ -33,7 +36,18 @@ class ExpandableFabButtonWidget extends StatelessWidget {
         ButtonWidget(
           text: StringsManager.chat,
           actionButton: FloatingActionButtonWidget(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(
+                AppRouter.kChatPage,
+                extra: {
+                  'name': statueModel.name,
+                  'contextName': figures[statueModel.name],
+                  'query': all_historical_figures[statueModel.name]
+                          ?["responses"] ??
+                      {},
+                },
+              );
+            },
             icon: AssetsManager.chatIcon,
           ),
         ),
